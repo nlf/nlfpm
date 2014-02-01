80f94697cba052adb3fcac42b315c3e64d9dfade
Nathan LaFreniere <quitlahok@gmail.com>
2014-02-01 12:44:11 -0800

    pew pew, everything works

--- index.js
+++ index.js
@@ -1,96 +1,80 @@
 var Hoek = require('hoek');
-var Proxy = require('./lib/proxy');
-var internals = {};
-var not_found = { error: 'not_found', reason: 'document not found' };
-
-internals.indexFailed = function (request, reply, err, settings) {
-    reply(not_found).code(404);
-};
-
-internals.getMeta = function (request, reply) {
-    this.getMeta(request.params.package, request.params.version, request.headers['if-none-match'], function (data, code, etag) {
-        reply(data).code(code).header('Etag', etag);
-    });
-};
-
-internals.getTarball = function (request, reply) {
-    if (!request.params.tarball.match(/\.tgz$/)) {
-        this.getMeta(request.params.package, request.params.version, request.params.tarball, request.query, request.headers['if-none-match'], function (data, code, etag) {
-            reply(data).code(code).header('Etag', etag);
-        });
-    } else {
-        this.getData(request.params.package, request.params.tarball, request.headers['if-none-match'], function (stream, code, etag) {
-            reply(stream).code(code).header('Etag', etag);
-        });
-    }
-};
+var Cache = require('./lib/cache');
 
 exports.register = function (plugin, options, next) {
-    var assert, nlfpm, proxy;
+    var nlfpm, cache;
 
     // validate our options
     Hoek.assert(typeof options.registries === 'object', 'Registries option must be an object');
     Hoek.assert(typeof options.registries.public === 'string', 'Public registry option must be a string');
     Hoek.assert(typeof options.registries.private === 'string', 'Private registry option must be a string');
     Hoek.assert(options.hasOwnProperty('ssl') && typeof options.ssl === 'boolean', 'SSL option must be a boolean');
-    Hoek.assert(typeof options.cache === 'object', 'Cache option must be an object');
-    Hoek.assert(typeof options.cache.redis === 'object', 'Redis option must be an object');
-    Hoek.assert(typeof options.cache.redis.host === 'string', 'Redis host option must be a string');
-    Hoek.assert(typeof options.cache.redis.port === 'number', 'Redis port options must be a number');
+    Hoek.assert(typeof options.redis === 'object', 'Redis option must be an object');
+    Hoek.assert(typeof options.redis.host === 'string', 'Redis host option must be a string');
+    Hoek.assert(typeof options.redis.port === 'number', 'Redis port options must be a number');
+    Hoek.assert(typeof options.path === 'string', 'Path option must be a string');
 
-    // create a new proxy object and bind it to our routes
-    proxy = new Proxy(options);
-    plugin.bind(proxy);
+    plugin.log(['info', 'nlfpm'], 'Starting nlfpm..');
+
+    // create a new cache object and bind it to our routes
+    cache = new Cache(options);
+    plugin.bind(cache);
 
     // select the 'nlfpm' tag in case someone is using this server for more than one thing
     nlfpm = plugin.select('nlfpm');
 
+    var defaultHandler = {
+        host: options.registries.public,
+        port: 443,
+        protocol: 'https',
+        passThrough: true,
+    };
+
+    var cacheHandler = Hoek.applyToDefaults(defaultHandler, { postResponse: cache.handler });
+
     // add our routes
     nlfpm.route({
         method: 'GET',
-        path: '/',
+        path: '/{package}/{version?}',
         handler: {
-            proxy: {
-                host: options.registries.public,
-                port: 443,
-                protocol: 'https',
-                passThrough: true,
-                failureResponse: internals.indexFailed
-            }
+            proxy: cacheHandler
         }
     });
 
     nlfpm.route({
         method: 'GET',
-        path: '/{package}/{version?}',
-        handler: internals.getMeta
+        path: '/{package}/-/{tarball}',
+        handler: {
+            proxy: cacheHandler
+        }
     });
 
     nlfpm.route({
         method: 'GET',
-        path: '/{package}/{version}/{tarball}',
-        handler: internals.getTarball
+        path: '/-/all/{extra?}',
+        handler: {
+            proxy: cacheHandler
+        }
     });
 
     nlfpm.route({
         method: 'POST',
         path: '/{params*}',
         handler: {
-            proxy: {
-                host: options.registries.public,
-                port: 443,
-                protocol: 'https',
-                passThrough: true
+            proxy: defaultHandler
+        }
+    });
+
+    nlfpm.route({
+        method: 'PUT',
+        path: '/{params*}',
+        handler: cache.putHandler,
+        config: {
+            payload: {
+                allow: 'application/json'
             }
         }
     });
 
-    // nlfpm.route({
-    //     method: 'PUT',
-    //     path: '/{params*}',
-    //     handler: {
-    //     }
-    // });
-
-    next();
+    cache.primeSearch(plugin, next);
 };
